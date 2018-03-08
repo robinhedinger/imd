cfc472139b6e10b5c5b0117ee386d3e09de33348
Robin Hedinger <robin.hedinger@swisscom.com>
2018-03-08 07:43:08 +0000

    Updated 9 files via CloudCannon

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-		var things = [/*Koreanisch*/		'안녕하세요',/*For the Seraina*/ 
-					  /*Russisch*/			'Здравствуйте', 
-					  /*Französisch*/		'Bonjour', 
-					  /*Hindi*/				'नमस्ते', 
-					  /*Englisch*/			'Hello', 
-					  /*Chinesisch*/		'你好', 
-					  /*Spanisch*/			'Hola', 
+		var things = [/*Koreanisch*/		/*'안녕하세요',/*For the Seraina*/
+					  /*Russisch*/			/*'Привет',
+					  /*Französisch*/		'Bonjour',
+					  /*Hindi*/				/*'नमस्ते',
+					  /*Englisch*/			'Hello',
+					  /*Chinesisch*/		/*'你好',
+					  /*Spanisch*/			'Hola',
 					  /*Italienisch*/		'Ciao',
 					  /*Rätroromanisch*/	'Allegra',
 					  /*Deutsch*/			'Hallo',
 					  /*Schweizerdeutsch*/	'Hoi',
-					  /*Arabisch*/			'مرحبا',
-					  /*:)*/				'ヾ(＾-＾) '
+					  /*Arabisch*/			/*'مرحبا',
+					  /*:)*/				/*'ヾ(＾-＾) '
 					 ];
 
-		var thingsmobile = [ 
-					  /*Hindi*/				'नमस्ते', 
-					  /*Englisch*/			'Hello', 
-					  /*Chinesisch*/		'你好', 
-					  /*Spanisch*/			'Hola', 
+		var thingsmobile = [
+					  /*Hindi*/				/*'नमस्ते',
+					  /*Englisch*/			'Hello',
+					  /*Chinesisch*/		/*'你好',
+					  /*Spanisch*/			'Hola',
 					  /*Italienisch*/		'Ciao',
 					  /*Deutsch*/			'Hallo',
 					  /*Schweizerdeutsch*/	'Hoi'
-			
+
 					 ];
 function isTouchDevice() {
     return 'ontouchstart' in document.documentElement;
@@ -31,7 +31,7 @@ if (isTouchDevice()) {
 	window.addEventListener("deviceorientation", handleOrientation, true);
 function handleOrientation(event) {
     // those angles are in degrees
-    var alpha = event.alpha;  
+    var alpha = event.alpha;
     var beta = event.beta;
     var gamma = event.gamma;
 
@@ -39,11 +39,11 @@ function handleOrientation(event) {
     var betaR = beta / 180 * Math.PI;
     var gammaR = gamma / 180 * Math.PI;
     var spinR = Math.atan2(Math.cos(betaR) * Math.sin(gammaR), Math.sin(betaR));
-	
+
 	// convert to usefull output
 	var gammaF = gamma / 5;
 	var betaF = (beta - 60) / 5;
-	 
+
 	if(gammaF>10){
 		gammaF=9;
 	}
@@ -60,35 +60,35 @@ function handleOrientation(event) {
     var spin = spinR * 180 / Math.PI;
 	console.log(spin);
 	$('#title').css("transform", "translateX("+gammaF+"px"+")"+"translateY("+betaF+"px"+")");
-	
+
 	if(Math.round(gammaF)>= -1 && Math.round(gammaF)<= 1 && Math.round(betaF)>=-1 && Math.round(betaF)<=1){
 		var thingmobile = thingsmobile[Math.floor(Math.random()*thingsmobile.length)];
 		console.log(thingmobile);
-    	document.getElementById("titletext").innerHTML = thingmobile;   	
+    	document.getElementById("titletext").innerHTML = thingmobile;
 		document.getElementById("titletext2").innerHTML = thingmobile;
 	    $("#titletext2").css("color", "#370edb");
 	}
 	else{
-        $("#titletext2").css("color","#f76be7");
+        $("#titletext2").css("color","#4af2d6");
 	}
 }
 }
 else {
-$(document).ready(function(){ 
-	
+$(document).ready(function(){
+
 "use strict";
 $("#title").mousemove(function(e){
-	
+
 
 	if (e.pageX < ($(window).width() / 1.8) && e.pageX > ($(window).width() / 2.2) && e.pageY < ($(window).height() / 1.6) && e.pageY > ($(window).height() / 2.4)) {
 		var thing = things[Math.floor(Math.random()*things.length)];
 		console.log(thing);
-    	document.getElementById("titletext").innerHTML = thing;   	
+    	document.getElementById("titletext").innerHTML = thing;
 		document.getElementById("titletext2").innerHTML = thing;
-        $("#titletext2").css("color", "#370edb");
-	} 
+        $("#titletext2").css("opacity", "0");
+	}
   else{
-        $("#titletext2").css("color","#f76be7");
+        $("#titletext2").css("opacity","1");
   }
 });
 });
@@ -114,15 +114,107 @@ $("#title").mousemove(function(e){
           var newvalueY2 = height2 * pageY2 * -1 - 50;
           $('#title').css("transform", "translateX("+newvalueX+"px"+")"+"translateY("+newvalueY+"px"+")");
           $('#title2').css("transform", "translateX("+newvalueX2+"px"+")"+"translateY("+newvalueY2+"px"+")");
-	
-	
-	
+
+
+
 });
 });
-
-
-
 
 
 }
 
+function customize(){
+	if ($( "#customize" ).hasClass( "customize-extended" )) {
+		$("#customize").removeClass("customize-extended");
+	}
+	else{
+		$("#customize").addClass("customize-extended");
+	}
+
+}
+
+function removestate(){
+	$("#color1").removeClass("color-crumped");
+	$("#color2").removeClass("color-crumped");
+	$("#color3").removeClass("color-crumped");
+	$("#color1").removeClass("color-active");
+	$("#color2").removeClass("color-active");
+	$("#color3").removeClass("color-active");
+}
+function color1(){
+		removestate();
+	  changecolor1()
+		$("#color1").addClass("color-active");
+		$("#color2").addClass("color-crumped");
+		$("#color3").addClass("color-crumped")
+}
+
+function color2(){
+		removestate();
+	  changecolor2()
+		$("#color2").addClass("color-active");
+		$("#color1").addClass("color-crumped");
+		$("#color3").addClass("color-crumped");
+}
+
+function color3(){
+		removestate();
+	  changecolor3()
+		$("#color3").addClass("color-active");
+		$("#color1").addClass("color-crumped");
+		$("#color2").addClass("color-crumped");
+}
+
+function changecolor1(){
+	var primary= "pink"
+	var secondary="blue"
+	changecolor();
+
+function changecolor(){
+	$(".titletext2")
+	    .css( "color", secondary );
+	$("body")
+			 .css( "background", primary )
+			 .css( "transition", "0.2s");
+	$(".titletext")
+			.css( "color", primary)
+			.css("text-shadow", "0px 0px 20px "+primary);
+
+}
+}
+
+function changecolor2(){
+	var primary= "#001e66"
+	var secondary="#4af2d6"
+	changecolor();
+
+function changecolor(){
+	$(".titletext2")
+	    .css( "color", secondary );
+	$("body")
+			 .css( "background", primary )
+			 .css( "transition", "0.2s");
+	$(".titletext")
+			.css( "color", primary)
+			.css("text-shadow", "0px 0px 20px "+primary);
+
+}
+}
+
+function changecolor3(){
+	var primary= "#f9ea93"
+	var secondary="#00007c"
+	changecolor();
+
+function changecolor(){
+	$(".titletext2")
+	    .css( "color", secondary );
+	$("body")
+			 .css( "background", primary )
+			 .css( "transition", "0.2s");
+	$(".titletext")
+			.css( "color", primary)
+			.css("text-shadow", "0px 0px 20px "+primary);
+
+}
+}
